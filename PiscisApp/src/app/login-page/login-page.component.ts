072907bf5a6309e4d259af1a24b69b540cf54004
Edycar Reyes <edycarreyes@gmail.com>
2018-03-07 00:06:56 -0600

    se termina de crear el registro de usuarios

--- PiscisApp/src/app/login-page/login-page.component.ts
+++ PiscisApp/src/app/login-page/login-page.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
+import {AuthService} from '../auth.service';
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent implements OnInit {
-
-  constructor() { }
+public email: string;
+public password: string;
+  constructor(
+   public authService: AuthService,
+   public router: Router,
+  ) { }
 
   ngOnInit() {
   }
 
+  onSubmitAddUser(){
+    this.authService.registerUser(this.email, this.password)
+    .then((res)=>{
+      this.router.navigate(['/privado']);
+    }).catch((err)=>{
+      console.log(err);
+    });
+  }
 }
