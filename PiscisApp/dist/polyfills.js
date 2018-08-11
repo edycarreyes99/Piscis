ad0e4d5892ea7bbb9abc9f5f3368e251dc06224e
Edycar Reyes <edycarreyes@gmail.com>
2018-08-11 17:50:59 -0600

    Compilacion Realizada
    se realiza la compilacion de la aplicacion despues de rediseñar e implementar algunos cambios en la plataforma

--- PiscisApp/dist/polyfills.js
+++ PiscisApp/dist/polyfills.js
@@ -5433,7 +5433,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\edyca\Desktop\WEBS\Piscis\PiscisApp\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! C:\wamp64\www\Piscis\PiscisApp\src\polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
