3e33c7bd162500098ceb6a3136ed2834bda06e04
Edycar Reyes <edycarreyes@gmail.com>
2018-08-10 21:08:10 -0600

    Dropzone Realizado, falta su diseño

--- PiscisApp/src/app/app.module.ts
+++ PiscisApp/src/app/app.module.ts
@@ -1,7 +1,17 @@
+//Config Modules Start
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import {ReactiveFormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {MaterialModule} from './material.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {DataTablesModule} from 'angular-datatables';
+import { MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
+import { NgwWowModule } from 'ngx-wow';
+
+//Components Start
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -11,26 +21,29 @@ import { PrivadoPageComponent } from './privado-page/privado-page.component';
 import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
 import { HistorialPageComponent } from './historial-page/historial-page.component';
 import {DataChartsComponent} from './data-charts/data-charts.component';
-import {AuthService} from './auth.service';
-import {ReactiveFormsModule} from '@angular/forms';
-import {HttpModule} from '@angular/http';
-import {MaterialModule} from './material.module';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {AngularFireModule} from 'angularfire2';
 import { DetallesComponent } from './detalles/detalles.component';
-import {AngularFireDatabaseModule} from 'angularfire2/database';
-import {AngularFireAuthModule} from 'angularfire2/auth';
-import {environment} from '../environments/environment';
-import {AuthContentOnlyGuard} from './guards/auth-content-only.guard';
-import {AngularFirestoreModule} from 'angularfire2/firestore';
-import {DataTablesModule} from 'angular-datatables';
-import { MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
-import { NgwWowModule } from 'ngx-wow';
 import { ProfileDashboardComponent } from './profile-dashboard/profile-dashboard.component';
 import { MonitoreoComponent } from './monitoreo/monitoreo.component';
 import { AddUserComponent } from './profile-dashboard/add-user/add-user.component';
 import { SidenavDashboardComponent } from './profile-dashboard/sidenav-dashboard/sidenav-dashboard.component';
 import { RealtimeDataseComponent } from './realtime-datase/realtime-datase.component';
+
+//App Services Start
+import {AuthService} from './auth.service';
+import {environment} from '../environments/environment';
+import {AuthContentOnlyGuard} from './guards/auth-content-only.guard';
+
+//AngularFire2 Modules Start
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+import {AngularFireAuthModule} from 'angularfire2/auth';
+import {AngularFirestoreModule} from 'angularfire2/firestore';
+import { AngularFireStorageModule } from "angularfire2/storage";
+
+//Directives Start
+import { DropZoneDirective } from './profile-dashboard/drop-zone.directive';
+import { FileUploadComponent } from './profile-dashboard/file-upload/file-upload.component';
+
 export const firebaseConfig={
   apiKey: "AIzaSyBu_Yb1UXx6W12dkeSYvr7aj9ueNwj0NLQ",
   authDomain: "proyecto-robotica-35bed.firebaseapp.com",
@@ -55,7 +68,9 @@ export const firebaseConfig={
     MonitoreoComponent,
     AddUserComponent,
     SidenavDashboardComponent,
-    RealtimeDataseComponent
+    RealtimeDataseComponent,
+    DropZoneDirective,
+    FileUploadComponent
   ],
   imports: [
     BrowserModule,
@@ -69,7 +84,8 @@ export const firebaseConfig={
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFireAuthModule,
     AngularFireDatabaseModule,
-    AngularFirestoreModule,
+    AngularFirestoreModule.enablePersistence(),
+    AngularFireStorageModule,
     MatTableModule,
     MatPaginatorModule,
     MatSortModule,
