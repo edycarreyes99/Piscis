e3f9710e2be654eea576a6bd5c321db26fad3d39
Edycar Reyes <edycarreyes@gmail.com>
2019-01-30 14:35:46 -0600

    se realiza el primer deploy de firebase functions para pruebas

--- PiscisApp/functions/src/index.ts
+++ PiscisApp/functions/src/index.ts
@@ -1,5 +1,15 @@
 import * as functions from 'firebase-functions';
 
+exports.UploadToFirestore = functions.https.onRequest((req, res) => {
+    if (req.method === 'POST') {
+        res.send('Metodo POST');
+    } else if (req.method === 'GET') {
+        res.send('Method GET');
+    } else {
+        res.send('Invalid Method!');
+    }
+});
+
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
